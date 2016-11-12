d9ce37afc8ffd2ce67af40c15ba1d73733acd141
skyandsy <yus067@ucsd.edu>
2016-11-12 10:38:44 -0800

    initial communicate with cassandra

--- webserver/index.js
+++ webserver/index.js
@@ -1,23 +1,42 @@
-var express = require('express'),
+var port = 8080;
+var express = require('express');
+    router = express();
     http = require('http'),
-    redis = require('redis');
+    assert = require('assert')
 
-var router = express();
-
-var directory = require('cassandra-driver');
-console.log('Creating a directory client');
-var directoryclient = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'demo'});
-directoryclient.connect(function (err) {
-  assert.ifError(err);
+// Query the directory
+var cassandra = require('cassandra-driver');
+console.log('Creating a directory cassandra client');
+var cassandra_client = new cassandra.Client({ contactPoints: ['127.0.0.1']});
+cassandra_client.connect(function(err,result){
+    console.log('Cassandra connected');
 });
 
 // To DO:
-const query = "SELECT name, email, birthdate FROM users WHERE key = 'mick-jagger'";
+console.log('Get cassandra directory');
+var select_query = 'SELECT * FROM test_keyspace.test_table';
+router.get('/', function(req, res) {
+    	cassandra_client.execute(select_query, [], function(err, result) {
+        console.log('select all');
+        var first = result.rows[0];
+        console.log('id %s value %s', first.id, first.test_value);
+    });
+});
 
-router.get('/', directoryclient.execute(query, function (err, result) {
-  var user = result.first();
-  //The row is an Object with column names as property keys. 
-  console.log('My name is %s and my email is %s', user.name, user.email);
-}));
+console.log('Post cassandra directory');
+const insert_query = 'INSERT INTO test_table (id, test_value) VALUES (?, ?)';
+router.post('/id/:id/value/:value', function(req, res){
+    cassandra_client.execute(insert_query, [req.params.id, req.params.value],
+        function(err, result){
+            if(err){
+                res.status(404).send({msg: err});
+            } else{
+               var first = result.rows[0]
+               console.log('id %s value %s', first.id, first.test_value);
+            }
+        });
+});
 
-
+http.createServer(router).listen(port, function() {
+  console.log('Listening on port ' + port);
+});
